48b57cdfc08b7983478ae16d9a6ad59a9ee3409a
Satheesh <satheesh.maheshkumar@freshworks.com>
2019-12-13 01:46:15 +0530

    Update index.js

--- .github/actions/create_pr/index.js
+++ .github/actions/create_pr/index.js
@@ -1,17 +1,10 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
-
-
-async function construct_pr_description(github_cli, base_branch, head_branch) {
-
-
-}
-
-
 async function run() {
 
   try {
 
+    //TODO: get these from config
     var production_branch = "master"
     var staging_branch = "dev"
 
@@ -31,57 +24,82 @@ async function run() {
     //  return ;
     //}
 
+    var pr_base = payload_pr.base.ref
+    var pr_head = payload_pr.head.ref
 
-    var base = production_branch //pull from config
-    var head = payload_pr.base.ref
-    var log_prefix = `${head}->${base}`
-    var pr_body = `${payload_pr.title}(#${payload_pr.number})`
-
-
-
-    try {
-      console.debug(`${log_prefix} Fetching pull request`)
-      var prlist_resp = await github_cli.pulls.list({
+    if (pr_base == production_branch && pr_head == staging_branch) {
+      //dev->master
+      var release_resp = await github_cli.repos.createRelease({
         owner: repo.owner,
         repo: repo.repo,
-        base: base,
-        head: `${repo.owner}:${head}`,
-        state: "open",
-      })
-
-      var prs = prlist_resp.data
-      if (prs.length == 0) {
-        console.info(`${log_prefix} Pull request not found. So creating one`)
-
-        var prcreate_resp = await github_cli.pulls.create({
-          owner: repo.owner,
-          repo: repo.repo,
-          base: base,
-          head: head,
-          title: "test",
-          body: pr_body
-        })
-        console.info(`${log_prefix} Pull request created`)
+        tag_name: payload_pr.title, //TODO: change it to tag format
+        name: payload_pr.title,
+        body: payload_pr.body,
+        draft: false,
+        prerelease: false,
+        target_commitish: production_branch
+      });
+    }
+    else if (pr_base == staging_branch) {
+      if (pr_head == production_branch) {
+        //master->dev reverse merge
       }
       else {
-        var existing_pr = prs[0]
+        //feature->dev merge. Create Release PR
 
-        console.info(`${log_prefix} ${prs.length} pull requests found. ${existing_pr.number}`)
-        pr_body = `${existing_pr.body}\n${pr_body}`
+        var base = production_branch //pull from config
+        var head = payload_pr.base.ref
+        var log_prefix = `${head}->${base}`
+        var pr_body = `${payload_pr.title}(#${payload_pr.number})`
 
-        var prupdate_resp = await github_cli.pulls.update({
-          owner: repo.owner,
-          repo: repo.repo,
-          pull_number: prs[0].number,
-          body: pr_body
-        })
-        //update description
+        try {
+          console.debug(`${log_prefix} Fetching pull request`)
+          var prlist_resp = await github_cli.pulls.list({
+            owner: repo.owner,
+            repo: repo.repo,
+            base: base,
+            head: `${repo.owner}:${head}`,
+            state: "open",
+          })
+
+          var prs = prlist_resp.data
+          if (prs.length == 0) {
+            console.info(`${log_prefix} Pull request not found. So creating one`)
+
+            var prcreate_resp = await github_cli.pulls.create({
+              owner: repo.owner,
+              repo: repo.repo,
+              base: base,
+              head: head,
+              title: "Release:",
+              body: pr_body
+            })
+            console.info(`${log_prefix} Pull request created`)
+          }
+          else {
+            var existing_pr = prs[0]
+
+            console.info(`${log_prefix} ${prs.length} pull requests found. ${existing_pr.number}`)
+            pr_body = `${existing_pr.body}\n${pr_body}`
+
+            var prupdate_resp = await github_cli.pulls.update({
+              owner: repo.owner,
+              repo: repo.repo,
+              pull_number: prs[0].number,
+              body: pr_body
+            })
+            //update description
+          }
+        }
+        catch (e) {
+          console.error(e)
+          throw e;
+        }
       }
     }
-    catch (e) {
-      console.error(e)
-      throw e;
-    }
+
+
+
 
 
 
