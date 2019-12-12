1edbf531f59db7f484cc3ea56be48da79cdffa69
Satheesh <satheesh.maheshkumar@freshworks.com>
2019-12-12 15:38:31 +0530

    Update index.js

--- .github/actions/create_pr/index.js
+++ .github/actions/create_pr/index.js
@@ -9,14 +9,13 @@ try {
   console.table(context.payload)
   
   
-  #var payload_pr = payload.pull_request
-  #var is_merged = payload_pr.merged
-  #if (!is_merged) {
-  #  console.log(`This is not a merge`)
-  #  return ;
-  #}
+  //var payload_pr = payload.pull_request
+  //var is_merged = payload_pr.merged
+  //if (!is_merged) {
+  //  console.log(`This is not a merge`)
+  //  return ;
+  //}
 }
 catch (error) {
   core.setFailed(error.message);
 }
-~
