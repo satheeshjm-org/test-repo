21e85217823cdb47ba6f55fd103940e5220eecd0
Satheesh <satheesh.maheshkumar@freshworks.com>
2019-12-13 17:50:07 +0530

    [JM-f-2] Feature2 (#66)
    * Update README.md (#55)
    
    * Update release.yaml
    
    * d3 (#57)
    
    * Update pre_release.yaml
    
    * Update pre_release.yaml
    
    * Update release.yaml
    
    * Update release.yaml
    
    * Update pre_release.yaml
    
    * Update index.js
    
    * Update index.js
    
    * Update index.js
    
    * Update README.md (#58)
    
    * Update index.js
    
    * Update index.js
    
    * Update index.js
    
    * Update README.md (#59)
    
    * Update index.js
    
    * Update README.md (#60)
    
    * Update index.js
    
    * Update README.md (#61)
    
    * Update index.js
    
    * Update README.md (#62)
    
    * Update index.js
    
    * Update README.md (#63)
    
    * Update index.js
    
    * Update README.md (#64)
    
    * Update index.js
    
    * Update README.md (#65)
    
    * Update README.md

--- .github/actions/create_master_pr/index.js
+++ .github/actions/create_master_pr/index.js
@@ -4,12 +4,12 @@ async function run() {
 
   try {
 
-    //TODO: get these from config
-    var production_branch = "master"
-    var staging_branch = "dev"
 
     const context = github.context
     const github_cli = new github.GitHub(process.env.GITHUB_TOKEN)
+
+    var staging_branch = core.getInput('staging_branch')
+    var production_branch = core.getInput('production_branch')
 
     const payload = context.payload
     var repo = context.repo
@@ -20,90 +20,71 @@ async function run() {
     }
     var is_merged = payload_pr.merged
     if (!is_merged) {
-     console.info(`This is not a merge`)
-     return ;
+      console.info(`This is not a merge`)
+      return ;
     }
 
-    var pr_base = payload_pr.base.ref
-    var pr_head = payload_pr.head.ref
+    /**
+     * allowed PRs : (!master) -> dev
+     *  1. if base_branch is not staging, dont run (This won't be needed, if you have proper conditions in the workflow config)
+     *  2. if head_branc is master, don't run (this is just a reverse merge)
+     */
+    if(payload_pr.base.ref != staging_branch) {
+      console.info(`Require base:${production_branch} for creating a release`)
+    }
+    if(payload_pr.head.ref == production_branch) {
+      console.info(`Require head:${staging_branch} for creating a release`)
+      return
+    }
 
-    if (pr_base == production_branch && pr_head == staging_branch) {
-      //dev->master
-      var release_resp = await github_cli.repos.createRelease({
+    var base = production_branch //pull from config
+    var head = payload_pr.base.ref
+    var log_prefix = `${head}->${base}`
+    var pr_body = `${payload_pr.title}(#${payload_pr.number})`
+
+    try {
+      console.debug(`${log_prefix} Fetching pull request`)
+      var prlist_resp = await github_cli.pulls.list({
         owner: repo.owner,
         repo: repo.repo,
-        tag_name: payload_pr.title, //TODO: change it to tag format
-        name: payload_pr.title,
-        body: payload_pr.body,
-        draft: false,
-        prerelease: false,
-        target_commitish: production_branch
-      });
-    }
-    else if (pr_base == staging_branch) {
-      if (pr_head == production_branch) {
-        //master->dev reverse merge
+        base: base,
+        head: `${repo.owner}:${head}`,
+        state: "open",
+      })
+
+      var prs = prlist_resp.data
+      if (prs.length == 0) {
+        console.info(`${log_prefix} Pull request not found. So creating one`)
+
+        var prcreate_resp = await github_cli.pulls.create({
+          owner: repo.owner,
+          repo: repo.repo,
+          base: base,
+          head: head,
+          title: "Release:",
+          body: pr_body
+        })
+        console.info(`${log_prefix} Pull request created`)
       }
       else {
-        //feature->dev merge. Create Release PR
+        var existing_pr = prs[0]
 
-        var base = production_branch //pull from config
-        var head = payload_pr.base.ref
-        var log_prefix = `${head}->${base}`
-        var pr_body = `${payload_pr.title}(#${payload_pr.number})`
+        console.info(`${log_prefix} ${prs.length} pull requests found. ${existing_pr.number}`)
+        pr_body = `${existing_pr.body}\n${pr_body}`
 
-        try {
-          console.debug(`${log_prefix} Fetching pull request`)
-          var prlist_resp = await github_cli.pulls.list({
-            owner: repo.owner,
-            repo: repo.repo,
-            base: base,
-            head: `${repo.owner}:${head}`,
-            state: "open",
-          })
-
-          var prs = prlist_resp.data
-          if (prs.length == 0) {
-            console.info(`${log_prefix} Pull request not found. So creating one`)
-
-            var prcreate_resp = await github_cli.pulls.create({
-              owner: repo.owner,
-              repo: repo.repo,
-              base: base,
-              head: head,
-              title: "Release:",
-              body: pr_body
-            })
-            console.info(`${log_prefix} Pull request created`)
-          }
-          else {
-            var existing_pr = prs[0]
-
-            console.info(`${log_prefix} ${prs.length} pull requests found. ${existing_pr.number}`)
-            pr_body = `${existing_pr.body}\n${pr_body}`
-
-            var prupdate_resp = await github_cli.pulls.update({
-              owner: repo.owner,
-              repo: repo.repo,
-              pull_number: prs[0].number,
-              body: pr_body
-            })
-            //update description
-          }
-        }
-        catch (e) {
-          console.error(e)
-          throw e;
-        }
+        var prupdate_resp = await github_cli.pulls.update({
+          owner: repo.owner,
+          repo: repo.repo,
+          pull_number: prs[0].number,
+          body: pr_body
+        })
+        //update description
       }
     }
-
-
-
-
-
-
-
+    catch (e) {
+      console.error(e)
+      throw e;
+    }
 
   }
   catch (error) {
--- .github/actions/create_release/index.js
+++ .github/actions/create_release/index.js
@@ -1,18 +1,20 @@
-
 const core = require('@actions/core');
 const github = require('@actions/github');
 async function run() {
 
   try {
 
-   
+
 
     const context = github.context
     const github_cli = new github.GitHub(process.env.GITHUB_TOKEN)
 
     const payload = context.payload
     var repo = context.repo
-    
+
+    var staging_branch = core.getInput('staging_branch')
+    var production_branch = core.getInput('production_branch')
+
 
     var payload_pr = payload.pull_request
     if(!payload_pr) {
@@ -20,30 +22,76 @@ async function run() {
     }
     var is_merged = payload_pr.merged
     if (!is_merged) {
-     console.info(`This is not a merge`)
-     return ;
+      console.info(`This is not a merge`)
+      return ;
     }
 
-    var production_branch = payload_pr.base.ref
-    var staging_branch = "dev"
-    var pr_head = payload_pr.head.ref
+    /**
+     * allowed PR : staging->master
+     *  1. if base_branch is not master, dont run (this won't be needed, if you have proper conditions in the workflow config)
+     *  2. if head_branch is not staging, don't run
+     */
+    //
+    if(payload_pr.base.ref != production_branch) {
+      console.info(`Require base:${production_branch} for creating a release`)
+    }
+    if(payload_pr.head.ref != staging_branch) {
+      console.info(`Require head:${staging_branch} for creating a release`)
+      return
+    }
 
-    if (pr_head == staging_branch) {
-      //dev->master
-      var release_resp = await github_cli.repos.createRelease({
+
+    //calculate body
+    var release_body = `${payload_pr.title}(#${payload_pr.number})\n\n${payload_pr.body}`
+
+    //calculate tag
+    const pr_title = payload_pr.title //title will be of the format Release: x.x.x.x
+    const regex_match = pr_title.match(/^Release:((\d+\.){3}\d+)$/m) //todo: move this to inputs
+    if(!regex_match) {
+      throw new Error(`Invalid Title. Expected format "Release:x.x.x.x" Actual value ${pr_title} `)
+    }
+    var release_tag = regex_match[1]
+
+
+    var existing_release
+    try {
+      console.debug(`Getting release by tag : ${release_tag}`)
+      var getrelease_resp = await github_cli.repos.getReleaseByTag({
         owner: repo.owner,
         repo: repo.repo,
-        tag_name: payload_pr.title, //TODO: change it to tag format
+        tag: release_tag,
+      })
+      console.debug(`Got release for tag : ${release_tag}`)
+      existing_release = getrelease_resp.data
+    }
+    catch (e) {
+      if (e.name == "HttpError" && e.status == 404) {
+        console.info(`Resource not found for ${e.request.url}`)
+      }
+      else {
+        throw e
+      }
+    }
+
+    if (!existing_release) {
+      console.debug(`Creating release for tag : ${release_tag}`)
+      var createrelease_resp = await github_cli.repos.createRelease({
+        owner: repo.owner,
+        repo: repo.repo,
+        tag_name: release_tag,
         name: payload_pr.title,
         body: payload_pr.body,
         draft: false,
         prerelease: false,
         target_commitish: production_branch
       });
+      console.debug(`Created release for tag : ${release_tag}`)
     }
     else {
-      //hotfix. branch->master
+      throw new Error(`Release tag exists`)
+
     }
+
   }
   catch (error) {
     core.setFailed(error.message);
