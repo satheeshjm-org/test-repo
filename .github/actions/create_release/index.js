aacca1aed1ea4dab37ebb24752d682c907738c8b
Satheesh <satheesh.maheshkumar@freshworks.com>
2019-12-13 14:50:39 +0530

    Update index.js

--- .github/actions/create_release/index.js
+++ .github/actions/create_release/index.js
@@ -1,18 +1,20 @@
-
 const core = require('@actions/core');
 const github = require('@actions/github');
 async function run() {
 
   try {
 
-   
+
 
     const context = github.context
     const github_cli = new github.GitHub(process.env.GITHUB_TOKEN)
 
     const payload = context.payload
     var repo = context.repo
-    
+
+    var staging_branch = core.getInput('staging_branch')
+    var production_branch = core.getInput('production_branch')
+
 
     var payload_pr = payload.pull_request
     if(!payload_pr) {
@@ -20,30 +22,37 @@ async function run() {
     }
     var is_merged = payload_pr.merged
     if (!is_merged) {
-     console.info(`This is not a merge`)
-     return ;
+      console.info(`This is not a merge`)
+      return ;
     }
 
-    var production_branch = payload_pr.base.ref
-    var staging_branch = "dev"
-    var pr_head = payload_pr.head.ref
+    /**
+     * allowed PR : staging->master
+     *  1. if base_branch is not master, dont run (this won't be needed, if you have proper conditions in the workflow config)
+     *  2. if head_branch is not staging, don't run
+     */
+    //
+    if(payload_pr.base.ref != production_branch) {
+      console.info(`Require base:${production_branch} for creating a release`)
+    }
+    if(payload_pr.head.ref != staging_branch) {
+      console.info(`Require head:${staging_branch} for creating a release`)
+      return
+    }
 
-    if (pr_head == staging_branch) {
-      //dev->master
-      var release_resp = await github_cli.repos.createRelease({
-        owner: repo.owner,
-        repo: repo.repo,
-        tag_name: payload_pr.title, //TODO: change it to tag format
-        name: payload_pr.title,
-        body: payload_pr.body,
-        draft: false,
-        prerelease: false,
-        target_commitish: production_branch
-      });
-    }
-    else {
-      //hotfix. branch->master
-    }
+
+
+    var release_resp = await github_cli.repos.createRelease({
+      owner: repo.owner,
+      repo: repo.repo,
+      tag_name: payload_pr.title, //TODO: change it to tag format
+      name: payload_pr.title,
+      body: payload_pr.body,
+      draft: false,
+      prerelease: false,
+      target_commitish: production_branch
+    });
+
   }
   catch (error) {
     core.setFailed(error.message);
