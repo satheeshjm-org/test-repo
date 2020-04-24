f4681b128c08724de3b75bb19cc07c83914cf5d7
jm <satheesh.jm@freshworks.com>
2020-04-24 16:48:52 +0530

    :shower:

--- statsd-telegraf/setup.py
+++ statsd-telegraf/setup.py
@@ -12,7 +12,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/freshdesk/system42-python-packages/tree/master/statsd-telegraf",
-    packages=["sstatsd-telegraf"],
+    packages=["statsd-telegraf"],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
