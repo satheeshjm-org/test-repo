c8ae6d453e6f84e4a51c4bf430547191fc97285a
jm <satheesh.jm@freshworks.com>
2020-04-24 16:52:10 +0530

    :shower:

--- statsd-telegraf/setup.py
+++ statsd-telegraf/setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="statsd-telegraf", # Replace with your own username
+    name="fw-statsd-telegraf", # Replace with your own username
     version="0.0.1",
     author="Satheesh JM",
     author_email="satheesh.maheshkumar@freshworks.com",
@@ -12,7 +12,8 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/freshdesk/system42-python-packages/tree/master/statsd-telegraf",
-    packages=["statsd-telegraf"],
+    packages=setuptools.find_packages(),
+    scripts = ["metrics_tracker.py"],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
