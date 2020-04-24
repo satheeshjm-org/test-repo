421ecfae62f9092575b9647731e54b09d10fb8dc
jm <satheesh.jm@freshworks.com>
2020-04-24 16:50:01 +0530

    :shower:

--- statsd-telegraf/setup.py
+++ statsd-telegraf/setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="fw-statsd-telegraf", # Replace with your own username
+    name="statsd-telegraf", # Replace with your own username
     version="0.0.1",
     author="Satheesh JM",
     author_email="satheesh.maheshkumar@freshworks.com",
