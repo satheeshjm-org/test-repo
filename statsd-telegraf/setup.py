709e083306a8eb49e0a54118fc9a74aeef08d378
jm <satheesh.jm@freshworks.com>
2020-04-24 15:18:32 +0530

    :shower:

--- statsd-telegraf/setup.py
+++ statsd-telegraf/setup.py
@@ -1,6 +1,6 @@
 import setuptools
 
-with open("system42-python-libs/statsd-telegraf/README.md", "r") as fh:
+with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
